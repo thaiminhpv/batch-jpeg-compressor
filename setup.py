b1ed67fa9b13dc86083986b64de95d6b98f78911
MinhDesktop <thaiminhpv@gmail.com>
2021-08-12 23:48:45 +0700

    build PyInstaller to get Executable file and add download_url to setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from distutils.core import setup
 
 setup(
-    name="batch_jpeg_compressor",  # How you named your package folder (MyLib)
-    packages=["batch_jpeg_compressor"],  # Chose the same as "name"
-    version="0.1",  # Start with a small number and increase it with every change you make
-    license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description="This is a CLI tool to compress multiple images (JPEG or PNG) at once using python",  # Give a short description about your library
-    author="thaiminhpv",  # Type in your name
-    author_email="thaiminhpv@gmail.com",  # Type in your E-Mail
-    url="https://github.com/thaiminhpv/batch-jpeg-compressor",  # Provide either the link to your github or to your website
-    download_url="blahblahblah",  # I explain this later on
+    name="batch_jpeg_compressor",
+    packages=["batch_jpeg_compressor"],
+    version="0.1",
+    license="MIT",
+    description="This is a CLI tool to compress multiple images (JPEG or PNG) at once using python",
+    author="thaiminhpv",
+    author_email="thaiminhpv@gmail.com",
+    url="https://github.com/thaiminhpv/batch-jpeg-compressor",
+    download_url="https://github.com/thaiminhpv/batch-jpeg-compressor/releases/download/v0.1/batch_jpeg_compressor",
     keywords=[
         "compressor",
         "jpeg",
         "jpg",
         "image processing",
-    ],  # Keywords that define your package best
-    install_requires=[  # I get to this in a second
+    ],
+    install_requires=[
         "Pillow",
         "tqdm",
     ],
     classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "Topic :: Image Processing :: Tools",
-        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
